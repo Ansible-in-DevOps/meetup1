2c7382eecaa4920816f07c59f11f22c5b7630bea
root <root@debian-greg.ctdp>
2019-10-15 21:11:21 +0200

    porzadki pod prelekcje

--- conf/entrypoint2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-     
-#start SSH server
-printf "Starting the SSH server."
-
-/etc/init.d/ssh start
-
-exec "$@"
--- conf/user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Adding user"
-
-useradd -m -d /home/${SSH_ANSIBLE_USER} -G ssh ${SSH_ANSIBLE_USER} -s /bin/bash
-echo "${SSH_ANSIBLE_USER}:${SSH_ANSIBLE_PASS}" | chpasswd
-echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_ANSIBLE_USER}/.profile
- 
-exec "$@"
